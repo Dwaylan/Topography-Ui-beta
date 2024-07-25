7180a2e0ec75c178bdfe4ec7624e0c5c2ac47ca6
Dwaylan <Dwaylan.Applewhite@gmail.com>
2024-07-25 15:56:52 -0400

    Chore: unified css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import SquareUI from "./assets/Shapes/square";
 import LinesUI from "./assets/Shapes/line";
+import "./app.css";
 
 const App = () => {
-  return <LinesUI />;
+  return (
+    <div>
+      <div id="info">The beginning</div>
+      <SquareUI />
+    </div>
+  );
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './app.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+// import './app.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <div>
-      <App/>
+      <App />
     </div>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
